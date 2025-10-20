6fb3c9132b87df9879eef6e4998f42229dc9003e
Lucas Alves <lucas1901alves@gmail.com>
2025-10-20 12:24:28 -0300

    style(skills)

--- barra_lateral.js
+++ barra_lateral.js
@@ -2,17 +2,16 @@ let btnBarraLat = document.getElementById('btn-barra-lateral')
 let barraLat = document.getElementById('barra-lateral')
 let overlay = document.getElementById('overlay-barra')
 
-btnBarraLat.addEventListener('click', ()=>{
+btnBarraLat.addEventListener('click', abrirBarra);
+barraLat.addEventListener('click', fecharBarra);
+overlay.addEventListener('click', fecharBarra);
+
+function abrirBarra() {
     barraLat.classList.add('abrir-barra');
-     overlay.style.display = 'block';
-});
+    overlay.style.display = 'block';
+}
 
-barraLat.addEventListener('click', ()=>{
-    barraLat.classList.remove('abrir-barra');
-     overlay.style.display = 'none';
-});
-
-overlay.addEventListener('click', ()=>{
+function fecharBarra() {
     barraLat.classList.remove('abrir-barra');
     overlay.style.display = 'none';
-});
+}
