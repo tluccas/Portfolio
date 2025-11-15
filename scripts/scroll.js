983e60dc415910ef32a731a069e30d709a2b7bd9
Lucas Alves <lucas1901alves@gmail.com>
2025-11-15 14:50:27 -0300

    refactor(portfolio): mudando o estilo de apresentação de projetos para cards com badges

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,12 +1,28 @@
 const divSobre = document.querySelector('.sobre .txt-sobre');
+const divPortfolio = document.querySelector(".portfolio-grid");
+const pPortfolio = document.querySelector(".portfolio p.port-desc");
+const portDesc = document.querySelector(".port-desc");
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting){
             entry.target.classList.add('animate');
             observer.unobserve(entry.target);
+
+             if (entry.target === divPortfolio) {
+               entry.target.addEventListener(
+                 "animationend",
+                 () => {
+                   portDesc.classList.add("animate");
+                 },
+                 { once: true }
+               ); 
+             }
         }
+
+
     });
 });
 
 observer.observe(divSobre);
+observer.observe(divPortfolio);
