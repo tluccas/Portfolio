36c73d43c7de98c7af2f06ba4d96983fc5c151c6
Lucas Alves <lucas1901alves@gmail.com>
2025-11-17 20:09:10 -0300

    style(sobre): animação

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,30 +1,28 @@
-const divSobre = document.querySelector('.sobre .txt-sobre');
-const divBadge = document.querySelector('.sobre .badge');
+const divImgSelfie = document.querySelector(".sobre .img-selfie");
+const divSobre = document.querySelector(".sobre .txt-sobre");
 const divPortfolio = document.querySelector(".portfolio-grid");
 const pPortfolio = document.querySelector(".portfolio p.port-desc");
 const portDesc = document.querySelector(".port-desc");
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting){
-            entry.target.classList.add('animate');
-            observer.unobserve(entry.target);
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+      observer.unobserve(entry.target);
 
-             if (entry.target === divPortfolio) {
-               entry.target.addEventListener(
-                 "animationend",
-                 () => {
-                   portDesc.classList.add("animate");
-                 },
-                 { once: true }
-               ); 
-             }
-        }
-
-
-    });
+      if (entry.target === divPortfolio) {
+        entry.target.addEventListener(
+          "animationend",
+          () => {
+            portDesc.classList.add("animate");
+          },
+          { once: true }
+        );
+      }
+    }
+  });
 });
 
-observer.observe(divBadge);
+observer.observe(divImgSelfie);
 observer.observe(divSobre);
 observer.observe(divPortfolio);
